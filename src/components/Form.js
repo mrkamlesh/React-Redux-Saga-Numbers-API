642d54d606e590ca34f5289e9e1fe23eed917e00
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-06-12 14:26:11 +0200

    wrote preliminary form

---
+++ src/components/Form.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Form(props) {
+  return (
+    <div>
+      <input type="text" placeholder="Please provide a number or type random" />
+      <button className="btn btn-primary">Click to get info</button>
+    </div>
+  )
+}
