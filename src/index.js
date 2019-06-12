c0943874ffceb4a50f38ea50a0b3f05caa279d84
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-06-12 19:39:38 +0200

    imported fontawesome

--- src/index.js
+++ src/index.js
@@ -1,19 +1,25 @@
 // import react
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 // import redux
-import { Provider } from 'react-redux'
+import { Provider } from "react-redux";
 // import the store
-import { store } from './store'
+import { store } from "./store";
 // import the main component
-import App from './components/App';
+import App from "./components/App";
 // import styles
-import './styles/index.css';
+import "./styles/index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 // import serviceWorker
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
